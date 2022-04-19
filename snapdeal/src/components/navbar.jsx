438d97532a59af340911eba579fccfc2ea119af2
Sandeep Kumar Behera <sandeepbeherakbl@gmail.com>
2022-04-19 18:51:57 +0530

    navbar

---
+++ snapdeal/src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import "./component.css";
+import logo from "./images/sdlogo.svg"
+
+export const Navbar = () => {
+    return(
+        <>
+            <div className="headdiv">
+                <div className="sub_headdiv">
+                    <div className="head-details">
+                        <p>Brand Waali Quality, Bazaar Waali Deal!</p>
+                    </div>
+                </div>
+            </div>
+            <div className="maindiv">
+                <div className="sub-div">
+                    <div className="logo">
+                        <img src={logo} alt="" srcset="" />
+                    </div>
+                    <div className="inputbox">
+                        <input type="text" placeholder="Search.." name="search" />
+                        <button type="search">Search</button>
+                    </div>
+                    <div className="login">
+                        <p>Cart</p>
+                        <p>signIn</p>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
---
+++ snapdeal/src/components/product.jsx
@@ -0,0 +1,9 @@
+export const Product = () =>{
+    return(
+        <>
+            <div>
+                <h1>This Is My Product</h1>
+            </div>
+        </>
+    )
+}
